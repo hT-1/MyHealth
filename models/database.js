6a92e1b1c6a6c84e28d4fac88e0cb3001763d4de
Luis Ramirez <langelramirez@ucla.edu>
2018-03-30 16:04:41 -0700

    deleted console.log

--- models/database.js
+++ models/database.js
@@ -42,10 +42,5 @@ db.query(newTablePeriodTxt, (err, sucess) => {
 
 
 
-// client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-//   console.log(err ? err.stack : res.rows[0].message) // Hello World!
-//   client.end()
-// })
-
 
 module.exports = db;
