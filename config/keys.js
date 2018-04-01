51a75c02a6b2a82397545666911c9b98b731b098
Esther Kim <estherkim@MacBook-Air-2075.local>
2018-03-31 17:04:44 -0700

    deleted config from gitignore so people can see

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+  google: {
+    clientID:'397760547443-9f28o0cukba11h13a1cepgv855ts4j97.apps.googleusercontent.com',
+    clientSecret:'4O_fYTQJXpuERiIV1Fs-8Hom'
+  },
+  postgres: {
+    dbURI: 'postgres://zwuvlpol:WoF8d1CEXDXq3y8fK1iBLWTw0n0eAg23@stampy.db.elephantsql.com:5432/zwuvlpol',
+  },
+  session: {
+    cookieKey: 'passspasspassthemar0und',
+  }
+}
